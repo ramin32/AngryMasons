d8d6ba546948a86d461eda090ea0a46f5e87c794
Ramin Rakhamimov <ramin32@gmail.com>
2014-02-24 08:08:22 -0500

    Episode 4

---
+++ AngryMasons-desktop/src/com/raminrakhamimov/angrymasons/Main.java
@@ -0,0 +1,16 @@
+package com.raminrakhamimov.angrymasons;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+
+public class Main {
+	public static void main(String[] args) {
+		LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
+		cfg.title = "Angry Masons " + AngryMasons.VERSION;
+		cfg.useGL20 = false;
+		cfg.width = 1280;
+		cfg.height = 700;
+		
+		new LwjglApplication(new AngryMasons(), cfg);
+	}
+}
---
+++ AngryMasons/src/com/raminrakhamimov/angrymasons/AngryMasons.java
@@ -0,0 +1,39 @@
+package com.raminrakhamimov.angrymasons;
+
+import com.badlogic.gdx.Game;
+import com.raminrakhamimov.angrymasons.screens.SplashScreen;
+
+public class AngryMasons extends Game {
+	public static final String VERSION = "0.1";
+	public static final String LOG = "Angry Masons";
+	
+	@Override
+	public void create() {	
+		setScreen(new SplashScreen(this));
+	}
+
+	@Override
+	public void dispose() {
+		super.dispose();
+	}
+
+	@Override
+	public void render() {		
+		super.render();
+	}
+
+	@Override
+	public void resize(int width, int height) {
+		super.resize(width, height);
+	}
+
+	@Override
+	public void pause() {
+		super.pause();
+	}
+
+	@Override
+	public void resume() {
+		super.resume();
+	}
+}
