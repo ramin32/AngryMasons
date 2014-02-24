f8719b6b896d08c42a93dac8728d0536ae4d58f5
Ramin Rakhamimov <ramin32@gmail.com>
2014-02-24 08:51:03 -0500

    Added Game screen

---
+++ AngryMasons/src/com/raminrakhamimov/angrymasons/screens/Game.java
@@ -0,0 +1,55 @@
+package com.raminrakhamimov.angrymasons.screens;
+
+import com.badlogic.gdx.Screen;
+import com.raminrakhamimov.angrymasons.AngryMasons;
+
+public class Game implements Screen {
+
+	private AngryMasons game;
+
+	public Game(AngryMasons game) {
+		this.game = game;
+	}
+	@Override
+	public void render(float delta) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void resize(int width, int height) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void show() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void hide() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void pause() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void resume() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void dispose() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
